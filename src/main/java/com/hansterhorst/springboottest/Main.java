b456ddf201422820b1b7cecd13eb8058e69e05ab
HansTH <hbhorst@mac.com>
2024-11-08 12:24:14 +0100

    setup project.

--- src/main/java/com/hansterhorst/springboottest/Main.java
+++ src/main/java/com/hansterhorst/springboottest/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTestApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTestApplication.class, args);
+        SpringApplication.run(Main.class, args);
+        System.out.println("Hello World!");
     }
 
 }
